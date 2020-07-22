e398ab2aa4cefd447748f38e41adf2210dc5956e
lesleyc@bu.edu <lesleyc@bu.edu>
2020-07-22 09:59:51 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+
+/*
+1. Create a HTTP Server. (express)
+*/
+
+/*
+2. Read file from local (fs with Promise)
+*/
+
+/*
+3. Handle HTTP Request.
+*/
+
+/*
+4. Pass file content to server via GET/POST request
+*/
+
+/*
+5. Save file content to server (fs with Promise)
+*/
+
+/*
+6. Handle HTTP Response.
+*/
+
+/*
+7. Retrieve file content from server storage for the first request
+*/
+
+/*
+8. Create middleware to cache (store) the response data. (using array)
+*/
+
+/*
+9. Retrieve file content from cache afterward
+*/
